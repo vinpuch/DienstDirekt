a2be08a24214abee387ec444eb699a06d367204d
Yusuf Uzun <yusuf3803uzun@gmail.com>
2024-06-03 14:11:40 +0200

    Login page done

--- .gradle/8.6/dependencies-accessors/4b7650b75e0815063ed8af2850c9b7999d3acbfb/sources/org/gradle/accessors/dm/LibrariesForLibs.java
+++ .gradle/8.6/dependencies-accessors/4b7650b75e0815063ed8af2850c9b7999d3acbfb/sources/org/gradle/accessors/dm/LibrariesForLibs.java
@@ -293,7 +293,7 @@ public class LibrariesForLibs extends AbstractExternalDependencyFactory {
         public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
 
         /**
-         * Version alias <b>agp</b> with value <b>8.4.0</b>
+         * Version alias <b>agp</b> with value <b>8.3.1</b>
          * <p>
          * If the version is a rich version and cannot be represented as a
          * single version string, an empty string is returned.
